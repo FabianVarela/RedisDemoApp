dba360f8a238269805f1b8079bf4ceb7f0545965
Fabian Varela Bonett <jfabian.303@gmail.com>
2018-09-01 14:16:40 -0500

    Implement the Redis Console client

--- RedisConsole/Program.cs
+++ RedisConsole/Program.cs
@@ -1,15 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RedisConsole
+﻿namespace RedisConsole
 {
+    using RedisManager.Class;
+    using RedisManager.Entity;
+    using System;
+    using System.Collections.Generic;
+
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var data = new CacheManager<List<Person>>();
+
+                if (data.Exists("people"))
+                {
+                    Console.WriteLine("Loading data, please wait... \n");
+
+                    var people = data.GetValue("people");
+
+                    foreach (var item in people)
+                        Console.WriteLine($"Document Id: {item.DocumentId}; Name: {item.Name}; Last Name: {item.LastName}; Address: {item.Address} \n");
+
+                    Console.WriteLine("Data loaded successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Writing data, please wait... \n");
+
+                    var people = new List<Person>
+                    {
+                        new Person
+                        {
+                            DocumentId = "1234567890",
+                            Name = "Jhon",
+                            LastName = "Doe",
+                            Address = "742 Evergreen Terrace"
+                        },
+                        new Person
+                        {
+                            DocumentId = "9876543210",
+                            Name = "Contoso",
+                            LastName = "Contoso",
+                            Address = "123 Fake Street"
+                        }
+                    };
+
+                    data.SetValue("people", people);
+
+                    Console.WriteLine("Data written successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
